3defba8a7f398eee04c1d6d8377e401b3f672110
Amanda Sutherland <amanda.h.sutherland.93@gmail.com>
2015-10-30 14:36:01 -0400

    adding files

---
+++ hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Simple Python program for SoftDes Toolbox Assignment Fall 2015.
+	Amanda Sutherland
+"""
+def hello_goodbye():
+	section1 = "I don't know why you say goodbye I say hello"
+	section2 = "hello, hello!"
+	section3 = "goodbye"
+	print (section2, section1, section2, section3)
+
+hello_goodbye()
